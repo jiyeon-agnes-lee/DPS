bbf6048605564c4d5f7d392640c883add6bc080c
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-07-23 22:57:16 +0900

    220723_solved : Programmers_Lv1_문자열 다루기 기본.java

---
+++ APS/Programmers/Java/Lv1/문자열 다루기 기본.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(String s) {
+        int len = s.length();
+        if(len == 4 || len == 6) {
+            for(int i =0; i < len; i++) {
+                if(s.charAt(i) < '0'|| s.charAt(i) > '9')
+                    return false;
+            }
+            return true;
+        }
+        else return false;
+    }
+}
