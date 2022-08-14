9b225544e1a33b721ce41f1e5e50a213afa69952
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-08-14 23:49:48 +0900

    220814_solved : Programmers_Lv1_2016년.java

---
+++ APS/Programmers/Java/Lv1/2016년.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        int date = 0;
+        for (int i = 0; i < a - 1; i++)
+            date += month[i];
+        
+        date += b - 1;
+
+        return days[date % 7];
+    }
+}
