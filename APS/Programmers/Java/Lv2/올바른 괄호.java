ef582c5b83234ba42c33d04ce86e62d6002a6379
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-08-22 22:00:57 +0900

    220822_solved : Prgrammers_Lv2_올바른 괄호.java

---
+++ APS/Programmers/Java/Lv2/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(i == 0 && c== ')')
+                return false;
+            if(stack.isEmpty()){
+                stack.push(c);
+                continue;
+            }
+            if(stack.peek() == '(' && c == ')')
+                stack.pop();
+            else 
+                stack.push(c);
+        }
+        if(!stack.isEmpty())
+            return false;
+        return answer;
+    }
+}
