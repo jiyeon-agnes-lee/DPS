587dbae0838d033361dcbcffbbee5d2af6640848
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-11-14 21:21:21 +0900

    221114_solved : Programmers_Lv1_부족한 금액 계산하기.java

---
+++ APS/Programmers/Java/Lv1/부족한 금액 계산하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long calcMoney = Long.valueOf(money);
+        for(int i = 1 ; i <= count; i++)
+            calcMoney -= price * i;
+        if(calcMoney < 0)
+            answer = Math.abs(calcMoney);
+        return answer;
+    }
+}
