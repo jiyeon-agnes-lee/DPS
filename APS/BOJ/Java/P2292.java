a72ae79fb85e4880570faa832b5954899a28404c
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-11-15 23:49:06 +0900

    221115_solved : BOJ_P2292.java

---
+++ APS/BOJ/Java/P2292.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int cnt = 1;
+		int num = 1;
+		while (true) {
+			if(num >= N)
+				break;
+			num += 6 * cnt;
+			cnt++;
+		}
+		System.out.println(cnt);
+		sc.close();
+	}
+}
