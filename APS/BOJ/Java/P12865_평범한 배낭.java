ff97d49b3900d0e9766165e871403e13de4b72f0
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-11-24 21:32:33 +0900

    221124_solved : BOJ_P12865_평범한 배낭.java

---
+++ APS/BOJ/Java/P12865_평범한 배낭.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] w = new int[n+1];
+        int[] v = new int[n+1];
+		int[][] dp = new int[n+1][k+1];
+		int maxV = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			w[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= k; j++) {
+				if(w[i] <= j) {
+					dp[i][j] = Math.max(v[i] + dp[i-1][j-w[i]], dp[i-1][j]);
+				}else {
+					dp[i][j] = dp[i-1][j];
+				}
+				maxV = Math.max(dp[i][j], maxV);
+			}
+		}
+		System.out.println(maxV);
+	}
+}
