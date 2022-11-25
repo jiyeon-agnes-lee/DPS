ea36bd1db2d102f1e895a3a3db4f6d3b414bf512
jiyeon-agnes-lee <jy.agnes.lee@gmail.com>
2022-11-25 22:54:56 +0900

    221125_solved : BOJ_P4949_균형잡힌 세상.java

---
+++ APS/BOJ/Java/P4949_균형잡힌 세상.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	private static Stack<Character> stack;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> arr = new ArrayList<>();
+		while(true) {
+			String str = sc.nextLine();
+			if(str.equals(".")) break;
+			arr.add(str);
+		}
+		char[] cArr;
+		
+		for(int i =0; i < arr.size(); i++) {
+            boolean flag = true;
+			stack = new Stack<>();
+			cArr = arr.get(i).toCharArray();
+			outer : for(int j = 0; j < cArr.length; j++) {
+				char c = cArr[j];
+				switch(c) {
+				case '(' :
+					stack.add(c);
+					break;
+				case ')':
+					flag = remove(c);
+					break;
+				case '[':
+					stack.add(c);
+					break;
+				case ']':
+					flag = remove(c);
+					break;
+				}
+                if(!flag) break outer;
+			}
+			if(!stack.empty()) flag = false;
+			if(flag) System.out.println("yes");
+			else System.out.println("no");
+			
+		}
+ 	}
+	public static boolean remove(char c) {
+		switch(c) {
+		case ')':
+			if(!stack.empty() && stack.peek() == '(') stack.pop();
+			else return false;
+			break;
+		case ']':
+			if(!stack.empty() && stack.peek() == '[') stack.pop();
+			else return false;
+			break;	
+		}	
+		return true;
+	}
+}
